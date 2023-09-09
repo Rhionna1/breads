373243563be3bbadb0179836a6ca0afe1e320079
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-09-09 13:00:08 -0500

    delete route complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,31 +1,32 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index ({breads}) {
-    return (
-      <Default>
-        <h2>Index Page</h2>
-        {/* This is a JSX comment. */}
-        {/*<p> I have {breads [0].name} bread!</p>*/}
-        <ul>
-            {
-              breads.map((bread, index)=> {
-                return(
-                    <li key={index}>
-                        <a href={`/breads/${index}`}>
-                         {bread.name}
-                        </a>
-                    </li>
-                )
-              })  
-            }
-        </ul>
-      </Default>
-    )
+function Index({ breads }) {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      {/* This is a JSX comment. */}
+      {/*<p> I have {breads [0].name} bread!</p>*/}
+      <ul>
+        {
+          breads.map((bread, index) => {
+            return (
+              <li key={index}>
+                <a href={`/breads/${index}`}>
+                  {bread.name}
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+      <div className="newButton">
+        <a href="/breads/new"><button>Add a new bread</button></a>
+      </div>
+    </Default>
+  )
 }
-<div className="newButton">
-  <a href="/breads/new"><button>Add a new bread</button></a>
-</div>
+
 
 
 module.exports = Index
