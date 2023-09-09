b8d1d6f6678442944927911aaedd22692c302a09
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-09-09 13:01:54 -0500

    finishing up delete form

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show ({bread, index}) {
+function Show({ bread, index }) {
 
   // Confirm we are getting our bread data in the terminal.
   // console.log(bread.name)
@@ -18,12 +18,13 @@ function Show ({bread, index}) {
         have gluten.
       </p>
       <img src={bread.image} alt={bread.name} />
-      
+
       <li><a href="/breads">Go home</a></li>
-      
+
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type='submit' value="DELETE" />
       </form>
+
 
     </Default>
 
